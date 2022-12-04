9109ccc0419413de808b4136be990306ae010d59
Fizoo <oleg.mykytchuk@gmail.com>
2022-12-04 14:16:28 +0200

    +

--- code/kuy 6/Are they the same.js
+++ code/kuy 6/Are they the same.js
@@ -13,6 +13,20 @@ const comp = (array1, array2) => {
 
     //your code here
 };
+
+
+//2
+/*function comp(a, b) {
+    return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
+}*/
+
+//3
+/*function comp(array1, array2) {
+    if(array1 == null || array2 == null) return false;
+    array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+    return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}*/
+
 a = [121, 144, 19, 161, 19, 144, 19, 11]
 b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 console.log(comp(a,b))
